f0cf89b24eee955bf10e677cef8701f5cec1df13
notlikelion <notlikelion@gmail.com>
2025-09-03 11:32:50 +0900

    쿠키유틸

--- src/main/java/com/example/springjwt/util/CookieUtil.java
+++ src/main/java/com/example/springjwt/util/CookieUtil.java
@@ -1,7 +1,10 @@
 package com.example.springjwt.util;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Optional;
 
 public class CookieUtil {
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
@@ -10,5 +13,8 @@ public class CookieUtil {
         cookie.setMaxAge(maxAge);
         cookie.setHttpOnly(true); // javascript 접근 금지 -> 유저와 서버만 볼 수 있게
         response.addCookie(cookie); // 응답에 추가
+    }
+
+    public static Optional<String> getCookie(HttpServletRequest request, String name) {
     }
 }
